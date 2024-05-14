51d37a02da8c989a7d642f284db8a70546496aa6
apsamade <bouderga.abdessamade@gmail.com>
2024-05-14 18:43:55 +0200

    maj

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,30 @@ import BackgroundAnime from "@components/BackgroundAnime"
 const Home = () => {
     return (
         <main>
-            <header className='min-h-[75vh] pt-32 bg-header relative'>
-                <div className='max-w-[800px] ml-64 mt-32'>
-                    <h1 className='text-4xl'>Portfolio Bouderga Abdel-samade <strong class="bg-gradient-to-r uppercase from-orange-300 to-orange-600 text-transparent bg-clip-text">Développeur web</strong></h1>
+            <header className='min-h-[75vh] pb-8 pt-16 md:pt-32 bg-header bg-cover bg-top relative z-10'>
+                <div className='max-w-[800px] lg:ml-64 lg:mt-32 mx-auto p-4'>
+                    <h1 className='text-3xl lg:text-4xl'>Portfolio Bouderga Abdel-samade <br /><strong class="bg-gradient-to-r uppercase from-orange-300 to-orange-600 text-transparent bg-clip-text">Développeur web</strong></h1>
                     <hr className='max-w-[100px] mt-8 mb-3' />
                     <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aperiam rerum atque, ad labore ipsa sit impedit expedita sed quam quaerat, obcaecati aspernatur optio eligendi reiciendis odio sequi deleniti accusantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, sapiente, perferendis soluta eum id.</p>
+                    <Link 
+                    href="/assets/CV_Bouderga_Abdel-samade_2.pdf"
+                    className="p-4 rounded-md uppercase hover:px-12 hover:bg-orange-600 duration-200 bg-orange-500 mt-4 block"
+                    target="_blank"
+                    >
+                        Mon CV
+                    </Link>
                 </div>
                 <BackgroundAnime />
             </header>
+            <section className="p-5 mx-auto max-w-[1200px] my-5">
+                <Image
+                    alt="Photo de Abdel-samade Bouderga Développeur web"
+                    src="/assets/elements/photo_presentation_2.jpg"
+                    width={500}
+                    height={500}
+                    className="rounded-md"
+                />
+            </section>
         </main>
     )
 }
--- components/BackgroundAnime.jsx
+++ components/BackgroundAnime.jsx
@@ -4,46 +4,46 @@ import Image from "next/image"
 
 const BackgroundAnime = () => {
     return (
-        <div className='absolute top-0 left-0 right-0 bottom-0'>
-                    <Image
-                        alt="Image de abdel-samade le développeur web"
-                        src="/assets/elements/rondorange.png"
-                        width={50}
-                        height={50}
-                        className="move-elem-1 absolute top-[30%] right-[25%] z-0"
+        <div className='absolute top-0 left-0 right-0 bottom-0 z-[-5]'>
+            <Image
+                alt="Image de abdel-samade le développeur web"
+                src="/assets/elements/rondorange.png"
+                width={50}
+                height={50}
+                className="move-elem-1 absolute top-[30%] right-[25%] z-0"
 
-                    />
-                    <Image
-                        alt="Image de abdel-samade le développeur web"
-                        src="/assets/elements/elem3.png"
-                        width={140}
-                        height={140}
-                        className="move-elem-4 absolute bottom-[25%] left-[10%] z-0"
+            />
+            <Image
+                alt="Image de abdel-samade le développeur web"
+                src="/assets/elements/elem3.png"
+                width={140}
+                height={140}
+                className="move-elem-4 absolute bottom-[25%] left-[10%] z-0"
 
-                    />
-                    <Image
-                        alt="Image de abdel-samade le développeur web"
-                        src="/assets/elements/elem2.png"
-                        width={120}
-                        height={120}
-                        className="move-elem-3 absolute bottom-[25%] right-[10%] z-0"
+            />
+            <Image
+                alt="Image de abdel-samade le développeur web"
+                src="/assets/elements/elem2.png"
+                width={120}
+                height={120}
+                className="move-elem-3 absolute bottom-[25%] right-[10%] z-0"
 
-                    />
-                    <Image
-                        alt="Image de abdel-samade le développeur web"
-                        src="/assets/elements/elem1.png"
-                        width={110}
-                        height={110}
-                        className="move-elem-2 absolute top-[25%] left-[25%] z-0"
-                    />
-                    <Image
-                        alt="Image de abdel-samade le développeur web"
-                        src="/assets/elements/elem1.png"
-                        width={110}
-                        height={110}
-                        className="move-elem-2 absolute top-[50%] left-[50%] z-0"
-                    />
-                </div>
+            />
+            <Image
+                alt="Image de abdel-samade le développeur web"
+                src="/assets/elements/elem1.png"
+                width={110}
+                height={110}
+                className="move-elem-2 absolute top-[25%] left-[25%] z-0"
+            />
+            <Image
+                alt="Image de abdel-samade le développeur web"
+                src="/assets/elements/elem1.png"
+                width={110}
+                height={110}
+                className="move-elem-2 absolute top-[50%] left-[50%] z-0"
+            />
+        </div>
     )
 }
 
